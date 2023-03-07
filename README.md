a7ff378e3ec8ff9015f47eba02ccb41530966b25
Shota Matsumoto <shota.matsumoto@lifematics.co.jp>
2023-03-07 12:08:30 +0900

    Add RefSeq table

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # gene-page
 
+
+
 ## Get started
 
+Requirements
+* Node.js 18.11.0 or above
+* Git LFS
+
 ```
+git clone git@github.com:orthordf/gene-page.git
+cd gene-page
 npm install
 npm start
 ```
--- controllers/geneController.js
+++ controllers/geneController.js
@@ -18,30 +18,60 @@ const headers = [
   "Feature_type"
 ];
 
-function createSummaryTable(geneId) {
+function getGeneInfo(geneId) {
   let fileContent = fs.readFileSync("gene_data/gene_info_tax9606.2022-09-30").toString();
   let geneInfo = Papa.parse(fileContent, {
     header: true
   });
+  let summaryTable = {}, symbol = '', description = '';
   for(let row of geneInfo.data) {
     if(row.GeneID == geneId) {
-      let summaryTable = {};
+      symbol = row['Symbol'];
+      description = row['description'];
       for(let header of headers) {
         summaryTable[header] = row[header];
       }
-      return summaryTable;
     }
   }
+  return [symbol, description, summaryTable];
+}
+
+
+function getRefseqInfo(geneId, seed) {
+  let fileContent = fs.readFileSync("gene_data/gene2refseq_tax9606").toString();
+  let gene2RefSeqInfo = Papa.parse(fileContent, {
+    header: true
+  });
+  let refseqStatusTable = {};
+  for(let row of gene2RefSeqInfo.data) {
+    if(row.GeneID == geneId) {
+      let RNAVersion = row['RNA_nucleotide_accession.version'];
+      let ProteinVersion = row['protein_accession.version'];
+      let status = row['status'];
+      if(RNAVersion !== '-' || ProteinVersion !== '-') {
+        refseqStatusTable[status] ||= {};
+        refseqStatusTable[status][RNAVersion] ||= new Set();
+        refseqStatusTable[status][RNAVersion].add(ProteinVersion);
+        seed[ProteinVersion] = 1; // TODO: What does '1' mean?
+      }
+    }
+  }
+  return refseqStatusTable;
 }
 
 const geneController = {
   async detail(req, res, next) {
     const id = req.params.id;
-    const summaryTable = createSummaryTable(id);
+    const [symbol, description, summaryTable] = getGeneInfo(id);
+    const seed = {};
+    const refseqStatusTable = getRefseqInfo(id, seed);
     res.render('gene/detail', {
       title: 'Gene info',
       id,
-      summaryTable
+      symbol,
+      description,
+      summaryTable,
+      refseqStatusTable
     });
   }
 };
