ed938ad6f852402b01907770d6a618892da3b482
Shota Matsumoto <shota.matsumoto@lifematics.co.jp>
2023-03-23 19:38:31 +0900

    Add BLAST plot (only for CCL3)

--- controllers/geneController.js
+++ controllers/geneController.js
@@ -38,6 +38,25 @@ async function getHomologenes(groupId) {
 }
 
 
+async function getBlastScores(symbol) {
+  let filePath = `gene_data/blast.scores/${symbol}.scores.txt`;
+  if(!fs.existsSync(filePath))
+     return null;
+  fileContent = fs.readFileSync(filePath).toString();
+  let blastRecords = Papa.parse(fileContent);
+
+  blastScores = blastRecords.data.map(record => {
+    return {
+      speciesIndex: record[0],
+      score: record[4],
+      isBBH: record[6] == 1,
+    }
+  });
+
+  return blastScores;
+}
+
+
 const geneController = {
   async detail(req, res, next) {
     const id = req.params.id;
@@ -45,6 +64,7 @@ const geneController = {
     const seed = {};
     const refseqStatusTable = await getRefseqInfo(id, seed);
     const homologenes = await getHomologenes(geneInfo.group_id);
+    const blastScores = await(getBlastScores(geneInfo.symbol));
 
     res.render('gene/detail', {
       title: 'Gene info',
@@ -53,7 +73,8 @@ const geneController = {
       description,
       geneInfo,
       refseqStatusTable,
-      homologenes
+      homologenes,
+      blastScores,
     });
   }
 };
