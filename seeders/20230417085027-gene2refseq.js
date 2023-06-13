2e8c1ee495250d04a257df219329a5405c6801b4
Shota Matsumoto <shota.matsumoto@lifematics.co.jp>
2023-06-13 11:33:07 +0900

    Update seeders/20230417085027-gene2refseq.js to import large tsv

--- seeders/20230417085027-gene2refseq.js
+++ seeders/20230417085027-gene2refseq.js
@@ -2,33 +2,88 @@
 
 const fs = require('fs');
 const Papa = require('papaparse');
+const readline = require('readline');
 /** @type {import('sequelize-cli').Migration} */
+
+async function insertGene2RefseqTSV(queryInterface, tsv) {
+  let gene2refseqs = Papa.parse(tsv, {
+    header: true,
+    delimiter: '\t'
+  });
+
+  let records = gene2refseqs.data.map((record) => {
+    let {
+      '#tax_id': tax_id,
+      GeneID: gene_id,
+      status,
+      'RNA_nucleotide_accession.version': rna_nucleotide_accession_version,
+      RNA_nucleotide_gi: rna_nucleotide_gi,
+      'protein_accession.versione': protein_accession_version,
+      protein_gi,
+      'genomic_nucleotide_accession.version': genomic_nucleotide_accession_version,
+      genomic_nucleotide_gi,
+      start_position_on_the_genomic_accession,
+      end_position_on_the_genomic_accession,
+      orientation,
+      assembly,
+      'mature_peptide_accession.version': mature_peptide_accession_version,
+      mature_peptide_gi,
+      Symbol: symbol
+    } = record;
+
+    return {
+      tax_id,
+      gene_id,
+      status,
+      rna_nucleotide_accession_version,
+      rna_nucleotide_gi,
+      protein_accession_version,
+      protein_gi,
+      genomic_nucleotide_accession_version,
+      genomic_nucleotide_gi,
+      start_position_on_the_genomic_accession,
+      end_position_on_the_genomic_accession,
+      orientation,
+      assembly,
+      mature_peptide_accession_version,
+      mature_peptide_gi,
+      symbol,
+      createdAt: Date.now(),
+      updatedAt: Date.now()
+    };
+  });
+
+  return queryInterface.bulkInsert('gene2refseqs', records).catch((err) => {
+    console.error(err);
+  });
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    let fileContent = fs.readFileSync('gene_data/gene2refseq').toString();
-    let gene2refseqs = Papa.parse(fileContent, {
-      header: false
+    const fileStream = fs.createReadStream('gene_data/gene2refseqs');
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity
     });
 
-    let records = gene2refseqs.data.map((record) => {
-      return {
-        gene_id: record[1],
-        symbol: record[2],
-        protein_id: record[3],
-        createdAt: new Date().toISOString(),
-        updatedAt: new Date().toISOString()
-      };
-    });
+    let fileContent = '';
+    let headerLine = null;
 
-    await queryInterface.bulkDelete('gene2refseqs', null, {
-      truncate: true,
-      cascade: true
-    });
+    for await (const line of rl) {
+      if (headerLine === null) {
+        headerLine = line + '\n';
+        continue;
+      } else {
+        fileContent += line + '\n';
+      }
 
-    // show error when bulkInsert failed
-    return queryInterface.bulkInsert('gene2refseqs', records).catch((err) => {
-      console.error(err);
-    });
+      if (fileContent.length >= 1e6) {
+        insertGene2RefseqTSV(queryInterface, headerLine + fileContent.trimEnd());
+        fileContent = '';
+      }
+    }
+
+    insertGene2RefseqTSV(queryInterface, headerLine + fileContent.trimEnd());
   },
 
   async down(queryInterface, Sequelize) {}
